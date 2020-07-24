1b305a921337794e5565acd784048250b5372a13
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-07-24 12:14:59 +0800

    MLC-346 Added sushi

--- src/Models/Colour.php
+++ src/Models/Colour.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Colour extends Model
 {
-    protected $guarded = [];
+    use \Sushi\Sushi;
 
-    protected $table = 'colours';
+    protected $rows = [
+        ['id' => 1, 'name' => 'pink'],
+        ['id' => 2, 'name' => 'red'],
+        ['id' => 3, 'name' => 'blue'],
+        ['id' => 4, 'name' => 'black'],
+        ['id' => 5, 'name' => 'light black'],
+        ['id' => 6, 'name' => 'dark black'],
+    ];
 }
--- tests/LaraveBasicSearchTest.php
+++ tests/LaraveBasicSearchTest.php
@@ -7,6 +7,8 @@ use JianJye\LaravelBasicSearch\LaravelBasicSearchServiceProvider;
 use JianJye\LaravelBasicSearch\Models\Colour;
 use Illuminate\Http\Request;
 use JianJye\LaravelBasicSearch\LaravelBasicSearch;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
 
 class LaraveBasicSearchTest extends TestCase
 {
@@ -16,30 +18,15 @@ class LaraveBasicSearchTest extends TestCase
         return [LaravelBasicSearchServiceProvider::class];
     }
 
-    protected function getEnvironmentSetUp($app)
-    {
-        include_once __DIR__.'/../src/database/migrations/create_colours_table.stub.php';
-        (new \CreateColoursTable)->up();
-    }
-
     /** @test */
     public function it_can_access_the_database()
     {
-        $colour = new Colour();
-        $colour->name = 'blue';
-        $colour->save();
-
-        $newColour = Colour::find($colour->id);
-
-        $this->assertSame($newColour->name, 'blue');
+        $this->assertEquals(6, count(Colour::all()));
     }
 
     /** @test */
     public function using_normal_search_function_success()
     {
-        factory(Colour::class, 5)->create();
-        factory(Colour::class)->create(['name' => 'rareColour']);
-
         $fields = ['name'];
         $ranges = [];
         $sorts = ['name', 'created_at',];
@@ -49,21 +36,17 @@ class LaraveBasicSearchTest extends TestCase
         $request = new Request();
         $request->setMethod('GET');
         $request->headers->set('key','value');
-        $request->query->add(['name' => 'rareColour']);
+        $request->query->add(['name' => 'red']);
 
         $colours = LaravelBasicSearch::fuzzySearch($request, $colours, $fields, $ranges, $sorts);
         $colours = $colours->first();
 
-        $this->assertSame($colours->name, 'rareColour');
+        $this->assertSame($colours->name, 'red');
     }
 
     /** @test */
     public function using_fuzzy_search_function_success()
     {
-        factory(Colour::class, 5)->create();
-        factory(Colour::class)->create(['name' => 'Dark rare']);
-        factory(Colour::class)->create(['name' => 'Light rare']);
-        factory(Colour::class)->create(['name' => 'Medium rare']);
 
         $fields = ['name'];
         $ranges = [];
@@ -74,11 +57,12 @@ class LaraveBasicSearchTest extends TestCase
         $request = new Request();
         $request->setMethod('GET');
         $request->headers->set('key','value');
-        $request->query->add(['name' => 'rare']);
+        $request->query->add(['name' => 'black']);
 
         $colours = LaravelBasicSearch::fuzzySearch($request, $colours, $fields, $ranges, $sorts);
         $colours = $colours->get();
 
         $this->assertEquals(3, count($colours));
     }
+
 }
