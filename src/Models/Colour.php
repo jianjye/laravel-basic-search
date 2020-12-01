79676105488ce9f923419d8593f5493c5e5a4af4
Farhan Hadi <farhan.abdhadi@gmail.com>
2020-12-01 11:34:29 +0800

    376: Update colour model for testing

--- src/Models/Colour.php
+++ src/Models/Colour.php
@@ -9,11 +9,11 @@ class Colour extends Model
     use \Sushi\Sushi;
 
     protected $rows = [
-        ['id' => 1, 'name' => 'pink'],
-        ['id' => 2, 'name' => 'red'],
-        ['id' => 3, 'name' => 'blue'],
-        ['id' => 4, 'name' => 'black'],
-        ['id' => 5, 'name' => 'light black'],
-        ['id' => 6, 'name' => 'dark black'],
+        ['id' => 1, 'name' => 'pink', 'date' => '2020-01-30'],
+        ['id' => 2, 'name' => 'red', 'date' => '2020-02-28'],
+        ['id' => 3, 'name' => 'blue', 'date' => '2020-03-27'],
+        ['id' => 4, 'name' => 'black', 'date' => '2020-04-29'],
+        ['id' => 5, 'name' => 'light black', 'date' => '2020-05-13'],
+        ['id' => 6, 'name' => 'dark black', 'date' => '2020-06-22'],
     ];
 }
